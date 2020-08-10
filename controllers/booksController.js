df5d29a15034b21027f290361d845c44aaac07e3
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-10 17:30:34 -0500

    find by id route

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -6,13 +6,19 @@ module.exports = {
             .find(req.query)
             .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .catch(err => res.status(500).json(err));
     },
     createBook(req, res) {
         db.Book
             .create(req.body)
             .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+            .catch(err => res.status(500).json(err));
+    },
+    findById(req, res) {
+        db.Book
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(500).json(err));
     }
 
 }
--- routes/api/books.js
+++ routes/api/books.js
@@ -6,4 +6,8 @@ router.route("/")
   .get(booksController.findAll)
   .post(booksController.createBook);
 
+// Matches with "/api/books/id"
+router.route("/:id")
+    .get(booksController.findById)
+
 module.exports = router;
