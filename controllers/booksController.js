0f74b990ff4c4bf9a90ed3b53c08a38d21ead241
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-10 17:20:00 -0500

    take out unnecessary code in find all route

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,19 +1,11 @@
 const db = require("../models");
 
 module.exports = {
-    // findAll(req, res) {
-    //     console.log("stuff")
-    //     db.Book
-    //         .find(req.query)
-    //         .sort({ date: -1 })
-    //         .then(dbModel => res.json(dbModel))
-    //         .catch(err => res.status(422).json(err));
-    // }
-    findAll: function(req, res) {
+    findAll(req, res) {
         db.Book
-          .find(req.query)
-          .sort({ date: -1 })
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.status(422).json(err));
-      }
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
 }
